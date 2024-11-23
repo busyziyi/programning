9497be15a1ff082a6e5c2a12ca73748b4b1e3e27
busyziyi <piziyi51@qq.com>
2024-11-23 12:26:45 +0800

    平方根

--- main.c
+++ main.c
@@ -1,26 +1,28 @@
-struct xinxi
-{
-    int number;
-    int grade;
-};
-#include<stdio.h>
-int main()
-{
-struct xinxi score[5];
-for(int i=0;i<5;i++)
-{
-    scanf("%d",&score[i].number);
-    scanf("%d",&score[i].grade);
+#include <stdio.h>
+#include <math.h>
+double sqrt(double x) {
+    if (x == 0) {
+        return 0;
+    }
+
+    double a = x;
+    double p = 1e-7;
+
+    do {
+        double b = 0.5 * (a + x / a);
+        if (fabs(b - a) < p) {
+            return b;
+        }
+        a = b;
+    } while (1);
 }
-int max=score[0].grade,m=0;
-for(int i=1;i<5;i++)
-{
-   if(max<score[i].grade)
-   {
-       max=score[i].grade;
-       m=i;
-   }
+int main() {
+    double x ;
+    scanf("%lf",&x);
+    double y = sqrt(x);
+
+    printf("%.7f\n",y);
+
+    return 0;
 }
-printf("%d %d",max,score[m].number);
-return 0;
-}
+
